8df230933f394338980f9280b98e258fccc097d2
21-manic-001g <21-manic-001g@21-manic-001>
2023-10-12 17:47:47 +0900

    1012

---
+++ Backjoon/src/B1900/q1978/Main.java
@@ -0,0 +1,21 @@
+package B1900.q1978;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		// 소수 1000 이하 숫자 1와 자기자신만 약수로 가짐
+
+	}
+
+}
--- Backjoon/src/B9000/q9012/Main2.java
+++ Backjoon/src/B9000/q9012/Main2.java
@@ -3,19 +3,43 @@ package B9000.q9012;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main2 {
-// 스택으로 풀어보기 '(' 를 스택에 push ')'를 만나면 pop 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		String X [] = new String[T];
-		
-		for(int i = 0; i<X.length; i++) {
-			X[i] = br.readLine();
-		}	
-		
-	}
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Stack<Integer> stack = new Stack<>();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(bf.readLine());
+
+		for (int i = 0; i < num; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			String s = st.nextToken();
+			stack.clear();
+
+			for (int j = 0; j < s.length(); j++) {
+				char c = s.charAt(j);
+
+				if (c == '(') {
+					stack.add(1);
+				}
+
+				else if (c == ')') {
+					if (!stack.isEmpty()) {
+						stack.pop();
+					} else {
+						stack.add(0);
+						break;
+					}
+				}
+			}
+			if (!stack.isEmpty() || stack.contains(0) != false) {
+				System.out.println("No");
+			} else {
+				System.out.println("YES");
+			}
+		}
+	}
 }
